dcb9ddfbb10486d99adda1ae42c06ad9b39b8534
Elías Grande <eliasgr89@gmail.com>
2015-04-05 11:50:01 +0200

    Updated JPA entities

--- master/master_db_ws/src/main/java/es/uem/cracking/master/jpa/Active_Attacks.java
+++ master/master_db_ws/src/main/java/es/uem/cracking/master/jpa/Active_Attacks.java
@@ -1,6 +1,15 @@
 package es.uem.cracking.master.jpa;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 
@@ -13,6 +22,186 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "ACTIVE_ATTACKS")
+@SequenceGenerator(name="a_a_seq", sequenceName="ACTIVE_ATTACKS_SEQ")
 public class Active_Attacks {
+	
+	// Private attributes
+	
+	/** Attack Id */
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="a_a_seq")
+	@Column(name = "id")
+	private long id;
+	/** Tool name */
+	@Column(name = "tool")
+	private String tool;
+	/** Attack type: hashing algorithms or network protocols */
+	@Column(name = "attack_type")
+	private String attackType;
+	/** Hydra remote user */
+	@Column(name = "remote_user")
+	private String remoteUser;
+	/** Hydra remote IP */
+	@Column(name = "remote_ip")
+	private String remoteIp;
+	/** Hydra remote port */
+	@Column(name = "remote_port")
+	private int remotePort;
+	/** Hydra or John extra parameters */
+	@Column(name = "extra_param")
+	private String extraParam;
+	/** John hash to crack */
+	@Column(name = "hash_to_crack")
+	private String hashToCrack;
+	/** Notify to email */
+	@Column(name = "notify_to_email")
+	private String notifyToEmail;
+	/** One to many */
+	@OneToMany(mappedBy="activeAttackId")
+	@OrderBy("id ASC")
+	private List<Attack_Windows> attackWindows;
+	
+	
+	// Getters and setters
+	
+	/**
+	 * Gets {@link #id}
+	 */
+	public long getId() {
+		return id;
+	}
+	
+	/**
+	 * Sets {@link #id}
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Gets {@link #tool}
+	 */
+	public String getTool() {
+		return tool;
+	}
+	
+	/**
+	 * Sets {@link #tool}
+	 */
+	public void setTool(String tool) {
+		this.tool = tool;
+	}
+	
+	/**
+	 * Gets {@link #attackType}
+	 */
+	public String getAttackType() {
+		return attackType;
+	}
+	
+	/**
+	 * Sets {@link #attackType}
+	 */
+	public void setAttackType(String attackType) {
+		this.attackType = attackType;
+	}
+	
+	/**
+	 * Gets {@link #remoteUser}
+	 */
+	public String getRemoteUser() {
+		return remoteUser;
+	}
+	
+	/**
+	 * Sets {@link #remoteUser}
+	 */
+	public void setRemoteUser(String remoteUser) {
+		this.remoteUser = remoteUser;
+	}
+	
+	/**
+	 * Gets {@link #remoteIp}
+	 */
+	public String getRemoteIp() {
+		return remoteIp;
+	}
+
+	/**
+	 * Sets {@link #remoteIp}
+	 */
+	public void setRemoteIp(String remoteIp) {
+		this.remoteIp = remoteIp;
+	}
+	
+	/**
+	 * Gets {@link #remotePort}
+	 */
+	public int getRemotePort() {
+		return remotePort;
+	}
+
+	/**
+	 * Sets {@link #remotePort}
+	 */
+	public void setRemotePort(int remotePort) {
+		this.remotePort = remotePort;
+	}
+
+	/**
+	 * Gets {@link #extraParam}
+	 */
+	public String getExtraParam() {
+		return extraParam;
+	}
+	
+	/**
+	 * Sets {@link #extraParam}
+	 */
+	public void setExtraParam(String extraParam) {
+		this.extraParam = extraParam;
+	}
+	
+	/**
+	 * Gets {@link #hashToCrack}
+	 */
+	public String getHashToCrack() {
+		return hashToCrack;
+	}
+
+	/**
+	 * Sets {@link #hashToCrack}
+	 */
+	public void setHashToCrack(String hashToCrack) {
+		this.hashToCrack = hashToCrack;
+	}
+
+	/**
+	 * Gets {@link #notifyToEmail}
+	 */
+	public String getNotifyToEmail() {
+		return notifyToEmail;
+	}
+
+	/**
+	 * Sets {@link #notifyToEmail}
+	 */
+	public void setNotifyToEmail(String notifyToEmail) {
+		this.notifyToEmail = notifyToEmail;
+	}
+	
+	/**
+	 * Gets {@link #attackWindows}
+	 */
+	public List<Attack_Windows> getAttackWindows() {
+		return attackWindows;
+	}
+	
+	/**
+	 * Sets {@link #attackWindows}
+	 */
+	public void setAttackWindows(List<Attack_Windows> attackWindows) {
+		this.attackWindows = attackWindows;
+	}
 
 }
--- master/master_db_ws/src/main/java/es/uem/cracking/master/jpa/Attack_Windows.java
+++ master/master_db_ws/src/main/java/es/uem/cracking/master/jpa/Attack_Windows.java
@@ -1,6 +1,15 @@
 package es.uem.cracking.master.jpa;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
@@ -12,7 +21,170 @@ import javax.persistence.Table;
  *
  */
 @Entity
+@IdClass(Attack_Windows_Id.class)
 @Table(name = "ATTACK_WINDOWS")
 public class Attack_Windows {
 
+	/** Active attack Id */
+	@Id
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="active_attack_id")
+	private Active_Attacks activeAttackId;
+	/** Attack window Id */
+	@Id
+	@Column(name = "id")
+	private long id;
+	/** Attemps */
+	@Column(name = "attemps")
+	private int attemps;
+	/** Sent timestamp */
+	@Column(name = "sent_timestamp")
+	private Timestamp sentTimestamp;
+	/** Brute force pattern */
+	@Column(name = "bf_pattern")
+	private String bfPattern;
+	/** Is window processed */
+	@Column(name = "processed")
+	private boolean processed;
+	/** Factor */
+	@Column(name = "factor")
+	private int factor;
+	/** First dictionary word to this window */
+	@OneToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="first_dictionary_word")
+	private Dictionary_Words firstDictionaryWord;
+	/** Last dictionary word to this window */
+	@OneToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="last_dictionary_word")
+	private Dictionary_Words lastDictionaryWord;
+	
+	
+	// Getters and setters
+	
+	/**
+	 * Gets {@link #activeAttackId}
+	 */
+	public Active_Attacks getActiveAttackId() {
+		return activeAttackId;
+	}
+	
+	/**
+	 * Sets {@link #activeAttackId}
+	 */
+	public void setActiveAttackId(Active_Attacks activeAttackId) {
+		this.activeAttackId = activeAttackId;
+	}
+	
+	/**
+	 * Gets {@link #id}
+	 */
+	public long getId() {
+		return id;
+	}
+	
+	/**
+	 * Sets {@link #id}
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Gets {@link #attemps}
+	 */
+	public int getAttemps() {
+		return attemps;
+	}
+	
+	/**
+	 * Sets {@link #attemps}
+	 */
+	public void setAttemps(int attemps) {
+		this.attemps = attemps;
+	}
+	
+	/**
+	 * Gets {@link #sentTimestamp}
+	 */
+	public Timestamp getSentTimestamp() {
+		return sentTimestamp;
+	}
+
+	/**
+	 * Sets {@link #sentTimestamp}
+	 */
+	public void setSentTimestamp(Timestamp sentTimestamp) {
+		this.sentTimestamp = sentTimestamp;
+	}
+
+	/**
+	 * Gets {@link #bfPattern}
+	 */
+	public String getBfPattern() {
+		return bfPattern;
+	}
+
+	/**
+	 * Sets {@link #bfPattern}
+	 */
+	public void setBfPattern(String bfPattern) {
+		this.bfPattern = bfPattern;
+	}
+
+	/**
+	 * Gets {@link #processed}
+	 */
+	public boolean isProcessed() {
+		return processed;
+	}
+
+	/**
+	 * Sets {@link #processed}
+	 */
+	public void setProcessed(boolean processed) {
+		this.processed = processed;
+	}
+
+	/**
+	 * Gets {@link #factor}
+	 */
+	public int getFactor() {
+		return factor;
+	}
+
+	/**
+	 * Sets {@link #factor}
+	 */
+	public void setFactor(int factor) {
+		this.factor = factor;
+	}
+
+	/**
+	 * Gets {@link #firstDictionaryWord}
+	 */
+	public Dictionary_Words getFirstDictionaryWord() {
+		return firstDictionaryWord;
+	}
+
+	/**
+	 * Sets {@link #firstDictionaryWord}
+	 */
+	public void setFirstDictionaryWord(Dictionary_Words firstDictionaryWord) {
+		this.firstDictionaryWord = firstDictionaryWord;
+	}
+
+	/**
+	 * Gets {@link #lastDictionaryWord}
+	 */
+	public Dictionary_Words getLastDictionaryWord() {
+		return lastDictionaryWord;
+	}
+
+	/**
+	 * Sets {@link #lastDictionaryWord}
+	 */
+	public void setLastDictionaryWord(Dictionary_Words lastDictionaryWord) {
+		this.lastDictionaryWord = lastDictionaryWord;
+	}
+	
 }
